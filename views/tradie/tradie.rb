7cffc0e41b3e5cc0f1237623179e70cebe48d5f8
samiulsyed <sam@devgenius.com.au>
2021-10-03 21:57:57 +1100

    colorise added to tradie views

--- views/tradie/tradie.rb
+++ views/tradie/tradie.rb
@@ -1,18 +1,18 @@
 require "./controllers/tradie_controller"
-
+require 'colorize'
 module Views
     module Tradies
         def self.new_tradie
-            puts "Please Enter your Business Details!"
-            print "Business Name: "
+            puts "Please Enter your Business Details!".red
+            print "Business Name: ".blue
             business_name = gets.chomp.strip
-            print "Please Enter Your Contact Number: "
+            print "Please Enter Your Contact Number: ".blue
             contact_number = gets.chomp.strip
-            print "What is your Trade? "
+            print "What is your Trade? ".blue
             trade = gets.chomp.strip
-            print "what is your email? "
+            print "what is your email? ".blue
             email = gets.chomp.strip
-            puts "Thank you we have added you to our List"
+            puts "Thank you we have added you to our List".green
             new_tradie_controller = TradieController.new
             new_tradie_controller.new_tradiec(business_name,contact_number,trade, email)
             
